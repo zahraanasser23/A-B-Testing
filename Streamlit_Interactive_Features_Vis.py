bab35ae82d76c5c626004679b88f76dde1851c1c
zahraanasser23 <136422348+zahraanasser23@users.noreply.github.com>
2023-09-25 22:13:31 +0300

    Update Streamlit_Interactive_Features_Vis.py

--- Streamlit_Interactive_Features_Vis.py
+++ Streamlit_Interactive_Features_Vis.py
@@ -9,8 +9,8 @@ import pandas as pd
 import plotly.graph_objs as go
 
 # Load your data for the control and test sets
-control_data = pd.read_csv('https://github.com/zahraanasser23/A-B-Testing/raw/main/control_group.csv')
-test_data = pd.read_csv('https://github.com/zahraanasser23/A-B-Testing/raw/main/test_group.csv')
+control_data = pd.read_csv("https://github.com/zahraanasser23/A-B-Testing/raw/main/control_group.csv")
+test_data = pd.read_csv("https://github.com/zahraanasser23/A-B-Testing/raw/main/test_group.csv")
 
 # Streamlit app title
 st.title('Clicks vs. Purchase Correlation')
