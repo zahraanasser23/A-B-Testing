6ba3c644169ff5634c812d1b9059e6fb646772da
zahraanasser23 <136422348+zahraanasser23@users.noreply.github.com>
2023-09-25 20:40:40 +0300

    scatterplot_streamlit.py

--- scatterplot_streamlit.py
+++ scatterplot_streamlit.py
@@ -1,24 +1,48 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-# Load your data for the control and test sets with the correct delimiter
-try:
-    control_data = pd.read_csv('control_group.csv', delimiter=';')
-    test_data = pd.read_csv('test_group.csv', delimiter=';')
-except Exception as e:
-    st.error(f"An error occurred while loading data: {str(e)}")
-    control_data = None
-    test_data = None
-
 # Check if data loaded successfully
 if control_data is not None and test_data is not None:
     # Streamlit app title
     st.title('Clicks vs. Purchase Correlation')
 
-    # ... (Rest of your Streamlit code)
+    # Sidebar filters (if needed)
+    st.sidebar.header('Filter Data')
+    # Add filter options if necessary
+
+    # Scatterplot
+    st.subheader('Scatterplot')
+    
+    # Create a scatterplot for control_data
+    fig_control = px.scatter(
+        control_data,
+        x='# of Website Clicks',
+        y='# of Purchase',
+        color_discrete_sequence=['blue'],
+        symbol='Campaign Name',
+        size='# of Website Clicks',
+        size_max=20,
+        title='Clicks vs. Purchase Correlation - Control Campaign'
+    )
+    
+    # Create a scatterplot for test_data
+    fig_test = px.scatter(
+        test_data,
+        x='# of Website Clicks',
+        y='# of Purchase',
+        color_discrete_sequence=['red'],
+        symbol='Campaign Name',
+        size='# of Website Clicks',
+        size_max=20,
+        title='Clicks vs. Purchase Correlation - Test Campaign'
+    )
+    
+    # Display the scatterplots
+    st.plotly_chart(fig_control, use_container_width=True)
+    st.plotly_chart(fig_test, use_container_width=True)
+
+    # Additional data analysis or visualizations can be added here
+
 else:
     st.error("Data loading failed. Please check your data files and format.")
 
 # Streamlit app footer (optional)
 st.sidebar.text("Powered by Streamlit and Plotly")
+
