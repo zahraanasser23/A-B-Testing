6aa8ea09d2f63b992a872857c9aecf373161592a
zahraanasser23 <136422348+zahraanasser23@users.noreply.github.com>
2023-09-25 22:52:19 +0300

    Update Streamlit_Interactive_Features_Vis.py

--- Streamlit_Interactive_Features_Vis.py
+++ Streamlit_Interactive_Features_Vis.py
@@ -2,19 +2,14 @@ import streamlit as st
 import plotly.graph_objs as go
 import pandas as pd
 
-campaign_name_filter = st.selectbox('Select Campaign Name', options=['All'] + control_group['Campaign'].unique().tolist())
-
-try:
-    control_group = pd.read_csv('control_group.csv')
-    test_group = pd.read_csv('test_group.csv')
-except Exception as e:
-    st.error(f"Error loading data: {str(e)}")
+control_group = pd.read_csv('https://github.com/zahraanasser23/A-B-Testing/raw/main/control_group.csv')
+test_group = pd.read_csv('https://github.com/zahraanasser23/A-B-Testing/raw/main/test_group.csv')
 
 # Create Streamlit app
 st.title('Marketing Campaigns A/B Testing')
 
 # Sidebar widgets for user interaction
-Campaign_name_filter = st.selectbox('Select Campaign Name', options=['All'] + control_group['Campaign Name'].unique().tolist())
+campaign_name_filter = st.selectbox('Select Campaign Name', options=['All'] + control_group['Campaign Name'].unique().tolist())
 spend_min = st.slider('Minimum Spend [USD]', min_value=0, max_value=int(control_group['Spend [USD]'].max()), value=0)
 spend_max = st.slider('Maximum Spend [USD]', min_value=0, max_value=int(control_group['Spend [USD]'].max()), value=int(control_group['Spend [USD]'].max()))
 
@@ -22,9 +17,9 @@ spend_max = st.slider('Maximum Spend [USD]', min_value=0, max_value=int(control_
 filtered_control_group = control_group.copy()
 filtered_test_group = test_group.copy()
 
-if Campaign_name_filter != 'All':
-    filtered_control_group = filtered_control_group[filtered_control_group['Campaign Name'] == Campaign_name_filter]
-    filtered_test_group = filtered_test_group[filtered_test_group['Campaign Name'] == Campaign_name_filter]
+if campaign_name_filter != 'All':
+    filtered_control_group = filtered_control_group[filtered_control_group['Campaign Name'] == campaign_name_filter]
+    filtered_test_group = filtered_test_group[filtered_test_group['Campaign Name'] == campaign_name_filter]
 
 filtered_control_group = filtered_control_group[(filtered_control_group['Spend [USD]'] >= spend_min) & (filtered_control_group['Spend [USD]'] <= spend_max)]
 filtered_test_group = filtered_test_group[(filtered_test_group['Spend [USD]'] >= spend_min) & (filtered_test_group['Spend [USD]'] <= spend_max)]
@@ -42,4 +37,5 @@ st.plotly_chart(box_plot_spend)
 
 # Streamlit app footer (optional)
 st.sidebar.text("Powered by Streamlit and Plotly")
+
 
