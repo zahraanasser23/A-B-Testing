03894b17e45652710af7c3fe29e7b48b2515c0f0
zahraanasser23 <136422348+zahraanasser23@users.noreply.github.com>
2023-09-25 22:27:45 +0300

    Update Streamlit_Interactive_Features_Vis.py

--- Streamlit_Interactive_Features_Vis.py
+++ Streamlit_Interactive_Features_Vis.py
@@ -3,72 +3,61 @@
 #test_data = pd.read_csv('https://github.com/zahraanasser23/A-B-Testing/raw/main/test_group.csv')
 
 
-
 import streamlit as st
-import pandas as pd
 import plotly.graph_objs as go
+import pandas as pd
 
 # Load your data for the control and test sets
-control_data = pd.read_csv('/kaggle/input/ab-testing-dataset/control_group.csv', delimiter=';')
-test_data = pd.read_csv('/kaggle/input/ab-testing-dataset/test_group.csv', delimiter=';')
+control_group = pd.read_csv('/kaggle/input/ab-testing-dataset/control_group.csv', delimiter=';')
+test_group = pd.read_csv('/kaggle/input/ab-testing-dataset/test_group.csv', delimiter=';')
 
-# Streamlit app title
-st.title('Clicks vs. Purchase Correlation')
 
-# Sidebar filters for Clicks vs. Purchase Correlation
-st.sidebar.header('Clicks vs. Purchase Correlation')
+# Create Streamlit app
+st.title('Marketing Campaigns A/B Testing')
 
-campaign_selection = st.sidebar.radio('Select Campaign', ('Control', 'Test'))
+# Create the box plot
+box_plot_spend = go.Figure()
 
-if campaign_selection == 'Control':
-    data = control_data
-    title = 'Control Campaign'
-else:
-    data = test_data
-    title = 'Test Campaign'
+box_plot_spend.add_trace(go.Box(x=control_group['Campaign Name'], y=control_group['Spend [USD]'], name='Control Campaign', boxpoints='all'))
+box_plot_spend.add_trace(go.Box(x=test_group['Campaign Name'], y=test_group['Spend [USD]'], name='Test Campaign', boxpoints='all'))
 
-x_column = st.sidebar.selectbox('X-axis', ['# of Website Clicks'])
-y_column = st.sidebar.selectbox('Y-axis', ['# of Purchase'])
+box_plot_spend.update_layout(title='Spend Comparison - Control vs. Test Campaign', xaxis_title='Campaign Name', yaxis_title='Spend [USD]')
 
-# Scatterplot for Clicks vs. Purchase Correlation
-fig = go.Figure()
+# Use st.plotly_chart to display the Plotly figure in Streamlit
+st.plotly_chart(box_plot_spend)
 
-fig.add_trace(go.Scatter(
-    x=data[x_column],
-    y=data[y_column],
-    mode='markers',
-    marker=dict(
-        size=12,
-        color='blue' if campaign_selection == 'Control' else 'red',
-        symbol='circle',
-        line=dict(
-            width=2,
-            color='DarkSlateGrey'
-        )
-    ),
-    name=title
-))
 
-fig.update_layout(
-    title=f'Clicks vs. Purchase Correlation - {title}',
-    xaxis_title=x_column,
-    yaxis_title=y_column
-)
+# Filter out rows with missing or invalid data in control campaign
+control_group_cleaned = control_group.dropna(subset=['# of Website Clicks', '# of Purchase'])
 
-# Sidebar filters for Spend Comparison
-st.sidebar.header('Spend Comparison')
+# Filter out rows with missing or invalid data in test campaign
+test_group_cleaned = test_group.dropna(subset=['# of Website Clicks', '# of Purchase'])
 
-# Assuming you have control_group and test_group DataFrames defined
-# Add the code for Spend Comparison here
+# Create scatter plots for Clicks vs. Purchase correlation for both campaigns
+scatter_control = px.scatter(control_group_cleaned, x='# of Website Clicks', y='# of Purchase',
+                             color_discrete_sequence=['blue'],
+                             symbol='Campaign Name',
+                             size='# of Website Clicks', size_max=20,
+                             title='Clicks vs. Purchase Correlation - Control Campaign')
 
-# Show the selected visualization (Clicks vs. Purchase Correlation or Spend Comparison)
-visualization_selection = st.sidebar.radio('Select Visualization', ['Clicks vs. Purchase Correlation', 'Spend Comparison'])
+scatter_test = px.scatter(test_group_cleaned, x='# of Website Clicks', y='# of Purchase',
+                          color_discrete_sequence=['red'],
+                          symbol='Campaign Name',
+                          size='# of Website Clicks', size_max=20,
+                          title='Clicks vs. Purchase Correlation - Test Campaign')
 
-if visualization_selection == 'Clicks vs. Purchase Correlation':
-    st.plotly_chart(fig, use_container_width=True)
-#else:
-    # Add the code for Spend Comparison visualization here
+# Combine the two scatter plots
+scatter_combined = px.scatter()
 
-# Streamlit app footer (optional)
-st.sidebar.text("Powered by Streamlit and Plotly")
+# Add traces from control and test scatter plots to the combined plot
+for trace in scatter_control.data:
+    scatter_combined.add_trace(trace)
+for trace in scatter_test.data:
+    scatter_combined.add_trace(trace)
 
+# Set layout for the combined plot
+scatter_combined.update_layout(title='Clicks vs. Purchase Correlation - Control vs. Test Campaign',
+                               xaxis_title='# of Website Clicks', yaxis_title='# of Purchase')
+
+# Use st.plotly_chart to display the Plotly figure in Streamlit
+st.plotly_chart(scatter_combined)
